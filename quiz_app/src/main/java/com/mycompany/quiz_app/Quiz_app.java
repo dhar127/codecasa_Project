0465818c5559431f6319e5aa3c118acd33ba9cca
DHARANI P <119476559+dhar127@users.noreply.github.com>
2023-10-24 08:45:02 +0530

    Add files via upload

---
+++ quiz_app/src/main/java/com/mycompany/quiz_app/Quiz_app.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quiz_app;
+
+/**
+ *
+ * @author dhara
+ */
+public class Quiz_app {
+
+    public static void main(String[] args) {
+        new Starts().setVisible(true);
+    }
+}
